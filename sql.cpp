ba3089bc4df0b0a26d6a15756ada6a2c142c9790
GitLab <pluthd@gmail.com>
2015-07-10 18:09:46 +0200

    keeps track of how many readers remain and deletes when this value hits zero; ;

--- sql.cpp
+++ sql.cpp
@@ -6,28 +6,35 @@
 using namespace std;
 
 string response;
+string old_readers;
 static int callback(void *NotUsed, int argc, char **argv, char **azColName){
     int i;
-  char* columnName;
-  char* columnValueString;
-  short int columnValueShortInt = 0;
-  int columnValueInt = 0;
+    char* columnName;
+    char* columnValueString;
+    short int columnValueShortInt = 0;
+    int columnValueInt = 0;
 
-//  cout << "begin of callback function\n";
+    //  cout << "begin of callback function\n";
 
-  for(i=0; i<argc; i++)
-  {
-    columnName = azColName[i];
-    if (strcmp(columnName, "MESSAGE")==0 || strcmp(columnName, "LastName")==0)
+    for(i=0; i<argc; i++)
     {
-      columnValueString = argv[i];
-      response = argv[i];
-    //  cout << "columnName = " << columnName << "; value = " << columnValueString <<"\n";
+        columnName = azColName[i];
+        if (strcmp(columnName, "MESSAGE")==0)
+        {
+            if (argv[i] != NULL)
+            response = argv[i];
+            else response="FALSE";
+        }
+        if (strcmp(columnName, "READERS")==0)
+        {
+            if (argv[i] != NULL)
+            old_readers = argv[i];
+            else response="FALSE";
+        }
+
     }
-    
-  }
-//  cout << "end of call back function \n";
-  return 0;
+    //  cout << "end of call back function \n";
+    return 0;
 }
 
 
@@ -49,21 +56,19 @@ int database_insert(string idhash, string message,string readers)
 
     /* Create SQL statement */
 
-     string  create = "CREATE TABLE IF NOT EXISTS MESSAGES("  \
-    "IDHASH         TEXT    NOT NULL," \
-        "MESSAGE        TEXT    NOT NULL," \
-        "READERS        TEXT    NOT NULL);";
+    string  create = "CREATE TABLE IF NOT EXISTS MESSAGES("  \
+                      "IDHASH         TEXT    NOT NULL," \
+                      "MESSAGE        TEXT    NOT NULL," \
+                      "READERS        TEXT    NOT NULL);";
 
     char *c_sql = new char[create.length() + 1];
     strcpy(c_sql, create.c_str());
     rc = sqlite3_exec(db, c_sql, callback, 0, &zErrMsg);
 
     /* Execute SQL statement */
-    //   char * sql = message
     string ssql = "INSERT INTO MESSAGES (IDHASH,MESSAGE,READERS) "  \
                    "VALUES ('"+idhash+"', '"+message+"', '"+readers+"' ); " ;
-    //   sql = "INSERT INTO MESSAGES (IDHASH,MESSAGE,READERS) "  \
-    "VALUES ('d93e06289f776316337adbcc851cdf07b362b6a7', 'd93e06289f776316337adbcc851cdf07b362b6a7', 'd93e06289f776316337adbcc851cdf07b362b6a7' ); " ;
+
     char *i_sql = new char[ssql.length() + 1];
     strcpy(i_sql, ssql.c_str());
     rc = sqlite3_exec(db, i_sql, callback, 0, &zErrMsg);
@@ -74,14 +79,15 @@ int database_insert(string idhash, string message,string readers)
         cout << "Table created successfully\n";
     }
     sqlite3_close(db);
-    //   cout << "Hello World!";
     return 0;
 }
 
 
 string database_retrieve(string idhash, string readers)
 {
+    response="";
 
+    string new_readers;
     sqlite3 *db;
     char *zErrMsg = 0;
     int  rc;
@@ -94,15 +100,32 @@ string database_retrieve(string idhash, string readers)
         cout << "Opened database successfully\n";
     }
 
-    /* Create SQL statement */
-//    cout << idhash << endl;
-     string  read = "select MESSAGE from messages where IDHASH='"+idhash+"';";
-     //string  read = "select MESSAGE from messages where idhash="+idhash;
-
-//    char *c_sql = new char[create.length() + 1];
-//    strcpy(c_sql, create.c_str());
+    string  read = "select MESSAGE,READERS from messages where IDHASH='"+idhash+"';";
     rc = sqlite3_exec(db, read.data(), callback, 0, &zErrMsg);
-//    string response="HI";
+
+    if ( response.empty() ) return "FALSE";
+
+    std::string delimiter = ",";
+
+    size_t pos = 0;
+    std::string token;
+    while ((pos = old_readers.find(delimiter)) != std::string::npos) {
+        token = old_readers.substr(0, pos);
+        old_readers.erase(0, pos + delimiter.length());
+        if (new_readers.empty() && token != readers ) new_readers+=token;
+        else if (token != readers) new_readers+=","+token;
+    }
+    if (old_readers != readers ) new_readers+= old_readers;
+    string  remove_readers = "update  messages set READERS='"+new_readers+"'where IDHASH='"+idhash+"';";
+    rc = sqlite3_exec(db, remove_readers.data(), callback, 0, &zErrMsg);
+
+
+    if ( new_readers.empty() ) {
+    string  deleterow = "delete from messages where IDHASH='"+idhash+"';";
+    rc = sqlite3_exec(db, deleterow.data(), callback, 0, &zErrMsg);
+    cout << "Row deleted" << endl;
+    }
+
     if( rc != SQLITE_OK ){
         cout << "SQL error: " << zErrMsg;
         sqlite3_free(zErrMsg);
