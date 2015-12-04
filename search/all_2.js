bfd3f1c39453ad254f2db12d1493cf0f030bb8d0
GitLab <pluthd@gmail.com>
2015-12-04 16:05:58 +0100

    added a bunch of info

--- search/all_2.js
+++ search/all_2.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['main',['main',['../server_8cxx.html#a0ddf1224851353fc92bfbff6f499fa97',1,'server.cxx']]]
+  ['main',['main',['../server_8cxx.html#a0ddf1224851353fc92bfbff6f499fa97',1,'main(int argc, char *argv[]):&#160;server.cxx'],['../objects_8h.html#acdef7a1fd863a6d3770c1268cb06add3',1,'main():&#160;objects.h']]]
 ];
